e07fdd455a074cea59c474b29c97337a5149aae1
tatoclemente <tatoapuros@gmail.com>
2023-07-11 11:20:03 -0300

    first commit

---
+++ el-festin/src/App.jsx
@@ -0,0 +1,11 @@
+import style from './App.module.css';
+
+function App() {
+  return (
+    <div className={style.appContainer}>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
