391812832ae7b5491d64f3ed3a82aa98e00b9a9c
Marcos Ramirez <gellollo@outlook.com>
2023-07-12 22:09:55 -0300

    Mapa por google maps realizado

--- el-festin/src/App.jsx
+++ el-festin/src/App.jsx
@@ -1,11 +1,13 @@
 import style from "./App.module.css";
 import { Navbar } from "./Components/NavBar/NavBar";
+import Maps from "./Views/Maps/Maps";
 
 function App() {
   return (
     <>
       <div className={style.appContainer}>
         <Navbar />
+        <Maps/>
       </div>
     </>
   );
