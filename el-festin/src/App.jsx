894364facc007cedb011679a68b4043d4253554b
NataliaRoni29 <rodriguezaisakataiga@gmail.com>
2023-07-12 20:23:47 -0500

    carrusel

--- el-festin/src/App.jsx
+++ el-festin/src/App.jsx
@@ -1,13 +1,11 @@
 import style from "./App.module.css";
 import { Navbar } from "./Components/NavBar/NavBar";
-import {Carousel} from "./Components/Carousel/Carousel"
 
 function App() {
   return (
     <>
       <div className={style.appContainer}>
         <Navbar />
-        <Carousel/>
       </div>
     </>
   );
