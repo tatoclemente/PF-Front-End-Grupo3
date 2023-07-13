c73be252b3ad3a396cc199cf7319c860071a1afe
Marcos Gallardi <gallardimarcos98@gmail.com>
2023-07-13 12:13:37 -0300

    Navbar semilisto

--- el-festin/src/Components/NavBar/SearchBar.jsx
+++ el-festin/src/Components/NavBar/SearchBar.jsx
@@ -7,17 +7,24 @@ export const SearchBar = () => {
     setInput(target.value);
   };
 
+  const onSubmitSearch = (e) => {
+    e.preventDefault();
+    console.log(input);
+  };
+
   return (
-    <div className="search-input-container">
+    <form onSubmit={onSubmitSearch} className="search-input-container">
       <input
         type="text"
         placeholder="Busca comidas, bebidas o postres..."
         className="mi-input fs-5 fw-bold"
         value={input}
         onChange={onInputChange}
-        />
-        <div className="search-line"></div>
-        <i className="fa-solid fa-magnifying-glass search-icon fs-3"></i>
-    </div>
+      />
+      <button
+        type="submit"
+        className="fa-solid fa-magnifying-glass border-end-0 border-top-0 border-bottom-0 border-dark search-icon fs-3"></button>
+      
+    </form>
   );
 };
