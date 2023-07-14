81088a49508b9b1248c82f0a7e2d430a7cae8f80
tatoclemente <tatoapuros@gmail.com>
2023-07-14 16:55:17 -0300

    Contenedor de categorias con slider

--- el-festin/src/Components/HomeComponents/FeaturedCategories/FeaturedCategories.jsx
+++ el-festin/src/Components/HomeComponents/FeaturedCategories/FeaturedCategories.jsx
@@ -1,28 +1,90 @@
-import style from './FeaturedCategories.module.css'
+import Style from './FeaturedCategories.module.css'
 import { dataDish, dataDrink } from '../../../utils/mock'
 import logo from '../../../images/default-image.jpg'
 
-const FeaturedCategories = () => {
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
+function SampleNextArrow(props) {
+
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", textShadow: '1px 1px 5px var(--shadow)' }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", textShadow: '1px 1px 5px var(--shadow)' }}
+      onClick={onClick}
+    />
+  );
+}
+
+const FeaturedCategories = () => {
   const subTypes = dataDish.map(dish => dish.subtype);
   const categories = [...new Set(subTypes)];
-  console.log(categories);
+
+  // Configuración del slider
+  const sliderSettings = {
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 7,
+      slidesToScroll: 4,
+      initialSlide: 0,
+      nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 6,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    };
+
+
   return (
-    <div className={style.mainContainer}>
-        <ul className={style.listContainer}>
-          {categories && categories.map((category, index) => {
-            return (<li key={index}>
-              <div className={style.categoryContainer}>
-                {console.log(category)}
-                <img className={style.imgCategory} src={logo} alt="imagen-default" />
-                <p>{category} </p>
-              </div>
-            </li>)
-          })}
-          
-        </ul>
+    <div className={Style.mainContainer}>
+      <Slider className={Style.listContainer} {...sliderSettings}>
+        {categories.map((category, index) => (
+            <div key={index} className={Style.categoryContainer}>
+              <img className={Style.imgCategory} src={logo} alt={category} />
+              <p>{category}</p>
+            </div>
+          ))}
+      </Slider>
     </div>
-  )
-}
+  );
+};
+
 
 export default FeaturedCategories
