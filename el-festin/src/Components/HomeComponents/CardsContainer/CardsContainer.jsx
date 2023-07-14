4f50ab022058ecf89ad26fe5b30fa62f7d5b74f5
tatoclemente <tatoapuros@gmail.com>
2023-07-14 20:05:16 -0300

    Componente Card y estilos al componente de FeatureCategories

--- el-festin/src/Components/HomeComponents/CardsContainer/CardsContainer.jsx
+++ el-festin/src/Components/HomeComponents/CardsContainer/CardsContainer.jsx
@@ -1,16 +1,27 @@
 import style from './CardsContainer.module.css'
 import { dataDish, dataDrinks } from '../../../utils/mock'
 import logo from '../../../images/default-image.jpg'
+import Card from '../../Card/Card'
 
+// {type, image, name, price, rating, description, id, addToCart}
 const CardsContainer = () => {
+  const addToCart = () => {
+    window.alert('Agregado al carrito')
+  }
   return (
     <div className={style.cardsContainer}>
       {dataDish.map((dish, index) => {
         return (
           <div className={style.card} key={index}>
-            <img src={logo} alt={dish.name} />
-            <h3>{dish.name}</h3>
-            <p>{dish.description}</p>
+            <Card 
+            type={dish.type}
+            image={logo}
+            name={dish.name}
+            price={dish.price}
+            rating={dish.rating}
+            description={dish.description}
+            id={dish.id}
+            addToCart={addToCart} />
           </div>
         )
       
