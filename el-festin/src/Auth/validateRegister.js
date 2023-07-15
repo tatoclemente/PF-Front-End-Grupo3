2c2dd7b8ad5e9292c52ed3847b21b9dc1e9fe1ac
FabianDom <fabiandmz13@gmail.com>
2023-07-15 17:36:21 -0500

    register-validacionFront

--- el-festin/src/Auth/validateRegister.js
+++ el-festin/src/Auth/validateRegister.js
@@ -1,32 +1,31 @@
-export default function Validate(form, pokemons) {
+export default function Validate(register) {
   let errors = {};
   let regex = /^[a-zA-Z\s]*$/;
-  let regexPhone = /^\+\d{8,11}$/;
+  let regexPhone = /^\d{1,14}$/;
   let regexEmail =
     /[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,5}/;
   let regexPassword = /^(?=\w*\d)(?=\w*)(?=\w*[a-z])\S{8,16}$/;
 
-  // Validar que solo se le pasen al input caracteres especificos
-  if (!regex.test(form.name)) {
+  if (!regex.test(register.name)) {
     errors.name = "Caracteres inválidos";
   }
-  //Validamos que sea un nombre corto no tenga espacios
-  if (form.name.length > 15) {
-    errors.name = "El nombre debe tener maximo 15 caracteres";
+
+  if (register.name.length > 20) {
+    errors.name = "El nombre debe tener maximo 20 caracteres";
   }
-  if (!regex.test(form.lastName)) {
+  if (!regex.test(register.lastName)) {
     errors.lastName = "Caracteres inválidos";
   }
-  if (form.lastName.length > 15) {
-    errors.lastName = "El apellido debe tener maximo 15 caracteres";
+  if (register.lastName.length > 25) {
+    errors.lastName = "El apellido debe tener maximo 25 caracteres";
   }
-  if (!regexPhone.test(phoneNumber)) {
+  if (!regexPhone.test(register.phoneNumber)) {
     errors.phoneNumber = "Numero de telefono invalido";
   }
-  if (!regexEmail.test(email)) {
+  if (!regexEmail.test(register.email)) {
     errors.email = "Dirección de correo electrónico no válida";
   }
-  if (!regexPassword.test(password)) {
+  if (!regexPassword.test(register.password)) {
     errors.password =
       "La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito";
   }
