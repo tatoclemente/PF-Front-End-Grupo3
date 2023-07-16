b7bb013991c0840a3e6d8049993df6b190edc5de
tatoclemente <tatoapuros@gmail.com>
2023-07-16 15:02:19 -0300

    buttons landing reinicia Sroll a 0

--- el-festin/src/Components/LandingComponents/Buttons/Buttons.jsx
+++ el-festin/src/Components/LandingComponents/Buttons/Buttons.jsx
@@ -1,18 +1,20 @@
-import style from './Buttons.module.css'
-import { Link } from 'react-router-dom'
+import style from "./Buttons.module.css";
+import { Link } from "react-router-dom";
+import { scrollToTop } from "../../../Helpers/functions";
 
 const Buttons = () => {
-
   return (
     <div className={style.container}>
-        <Link to="/home" style={{ textDecoration: "none" }}>
-<button className={style.button}>QUIERO HACER UN PEDIDO</button>
-</Link>
-<Link to="/booking" style={{ textDecoration: "none" }}>
-<button className={style.button}>QUIERO HACER UNA RESERVACION</button>
-</Link>
+      <Link to="/home" style={{ textDecoration: "none" }}>
+        <button onClick={() => scrollToTop()} className={style.button}>
+          QUIERO HACER UN PEDIDO
+        </button>
+      </Link>
+      <Link to="/booking" style={{ textDecoration: "none" }}>
+        <button onClick={() => scrollToTop()} className={style.button}>QUIERO HACER UNA RESERVACION</button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Buttons
+export default Buttons;
