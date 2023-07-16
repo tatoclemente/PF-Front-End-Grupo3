e4f18fd876db443cbfc7af5010f21fc25e58db05
Marcos Gallardi <gallardimarcos98@gmail.com>
2023-07-16 19:22:15 -0300

    comienzo del dashboard

---
+++ el-festin/src/Components/Dashboard/ModalCreateDrink.jsx
@@ -0,0 +1,141 @@
+import { useState } from "react";
+
+export const ModalCreateDrink = () => {
+  let initialState = {
+    name: "",
+    volume: "",
+    type: "",
+    alcohol: "",
+    stock: "",
+    price: "",
+    image: "",
+  };
+
+  const [inputCreateDrink, setInputCreateDrink] = useState(initialState);
+
+  const onInputChange = ({ target }) => {
+    setInputCreateDrink({
+      ...inputCreateDrink,
+      [target.name]: target.value,
+    });
+  };
+
+  const onSubmitCreate = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="container-fluid">
+      
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#staticBackdrop1">
+        Crear Bebida
+      </button>
+
+      <div
+        className="modal fade"
+        id="staticBackdrop1"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="staticBackdropLabel">
+                Crear bebida nuevo
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <form onSubmit={onSubmitCreate}>
+                <label htmlFor="" className="pe-3 pt-3 form-label">
+                  Nombre
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="name"
+                  value={inputCreateDrink.name}
+                  onChange={onInputChange}
+                />
+                <label htmlFor="" className="pe-3 pt-3 form-label">
+                  Medida
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="name"
+                  value={inputCreateDrink.volume}
+                  onChange={onInputChange}
+                />
+
+                <label htmlFor="" className="pe-3 pt-3 form-label">
+                  tipo de bebida
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="type"
+                  value={inputCreateDrink.type}
+                  onChange={onInputChange}
+                />
+                <label htmlFor="" className="pe-3 pt-3 form-label">
+                  Contiene alcohol
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="subtype"
+                  value={inputCreateDrink.alcohol}
+                  onChange={onInputChange}
+                />
+
+                <label htmlFor="" className="pe-3 pt-3 form-label">
+                  Stock
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="calories"
+                  value={inputCreateDrink.stock}
+                  onChange={onInputChange}
+                />
+
+                <label htmlFor="" className="pe-3 pt-3 form-label">
+                  Precio
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="price"
+                  value={inputCreateDrink.price}
+                  onChange={onInputChange}
+                />
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    data-bs-dismiss="modal">
+                    Cerrar
+                  </button>
+                  <button type="submit" className="btn btn-primary">
+                    Crear
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ el-festin/src/Components/Dashboard/ModalCreateSide.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+
+export const ModalCreateSide = () => {
+  let initialState = {
+    name: "",
+    stock: "",
+    price: "",
+    image: "",
+  };
+
+  const [inputCreateSide, setInputCreateSide] = useState(initialState);
+
+  const onInputChange = ({ target }) => {
+    setInputCreateSide({
+      ...inputCreateSide,
+      [target.name]: target.value,
+    });
+  };
+
+  const onSubmitCreate = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="container-fluid">
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#staticBackdrop3">
+        Crear Guarnicion
+      </button>
+
+      <div
+        className="modal fade"
+        id="staticBackdrop3"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="staticBackdropLabel">
+                Crear Guarnicion nuevo
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <form onSubmit={onSubmitCreate}>
+                <label htmlFor="" className="pe-3 pt-3 form-label">
+                  Nombre
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="name"
+                  value={inputCreateSide.name}
+                  onChange={onInputChange}
+                />
+
+                <label htmlFor="" className="pe-3 pt-3 form-label">
+                  Tipo
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="calories"
+                  value={inputCreateSide.stock}
+                  onChange={onInputChange}
+                />
+
+                <label htmlFor="" className="pe-3 pt-3 form-label">
+                  Precio
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="price"
+                  value={inputCreateSide.price}
+                  onChange={onInputChange}
+                />
+
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    data-bs-dismiss="modal">
+                    Cerrar
+                  </button>
+                  <button type="submit" className="btn btn-primary">
+                    Crear
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
