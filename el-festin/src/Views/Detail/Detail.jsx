1e5b078cd583a7f1385568393acce223808468a3
tatoclemente <tatoapuros@gmail.com>
2023-07-16 19:41:16 -0300

    Card conectada al datail y Detail en proceso

--- el-festin/src/Views/Detail/Detail.jsx
+++ el-festin/src/Views/Detail/Detail.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DetailPage from "../../Components/DetailPage/DetailPage"
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 function Detail() {
+  const { id } = useParams()
+  const [loading, setLoading] = useState(true)
+console.log(id);
+  const dishes = useSelector(state => state.dishes)
+  console.log(dishes);
+  const dishDetail = dishes.dishes.find(dish => dish.id == id)
+
+  console.log(dishDetail);
+
+  // useEffect(() => {
+  //   let isMounted = true;
+  //   const fetchData = async () => {
+  //     try {
+  //       const {data} = await axios.get(`/recipes/${id}`)
+  //       if(data.name) {
+  //         setRecipe(data);
+  //       }
+  //       window.scrollTo(0, 0);
+  //       setLoading(false)
+  //     } catch (error) {
+  //       if(isMounted){
+  //         history.push(ROUTE.NOT_FOUND)
+  //         setLoading(false)
+  //         console.log(error);
+  //       }
+  //     }
+  //   }
+  //   fetchData()
+
+  //   return () => {
+  //     isMounted = false; // Establecer la variable de control en false al desmontar el componente
+  //   };
+  // }, [id, history])
+
   return (
-    <div><DetailPage/></div>
+    <div><DetailPage
+      dishDetail={dishDetail} 
+      />
+    </div>
   )
 }
 
