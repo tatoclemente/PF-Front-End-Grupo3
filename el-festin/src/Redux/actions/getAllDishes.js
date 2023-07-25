8f3edd92265abd4f4f23ca1170f17f9b780ecc75
tatoclemente <tatoapuros@gmail.com>
2023-07-25 18:51:01 -0300

    Corrección de bug: Reemplace HTTP por HTTPS en actions de dish

--- el-festin/src/Redux/actions/getAllDishes.js
+++ el-festin/src/Redux/actions/getAllDishes.js
@@ -1,9 +1,10 @@
+import { server } from "../../Helpers/EndPoint";
 import { getAllDishes } from "../slices/platosSlice";
 import axios from "axios";
 
 export const getDishes = () => async (dispatch) => {
     try {
-      const { data } = await axios.get("http://pf-server-production.up.railway.app/dish");
+      const { data } = await axios.get(`${server}/dish`);
       dispatch(getAllDishes(data));
     } catch (error) {
       console.log(error.message);
