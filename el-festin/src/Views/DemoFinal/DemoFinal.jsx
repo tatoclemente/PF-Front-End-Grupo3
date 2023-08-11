b4187cca5c79d1c87375f22422b8addbb54e65cf
tatoclemente <tatoapuros@gmail.com>
2023-08-11 16:35:48 -0300

    Vista Demo Final

--- el-festin/src/Views/DemoFinal/DemoFinal.jsx
+++ el-festin/src/Views/DemoFinal/DemoFinal.jsx
@@ -3,12 +3,13 @@ import style from './DemoFinal.module.css'
 import { Link } from 'react-router-dom'
 import { FaRegCirclePlay } from 'react-icons/fa6'
 import logo from '../../Assets/logo-el-festin-nav.png'
+import ROUTES from '../../Routes/routes'
 
 function DemoFinal() {
   return (
     <div className={style.mainContainer}>
         <h1 className={style.title}>¡Bienvenidos!</h1>
-        <Link className={style.button} to={'https://pf-front-end-grupo3.vercel.app/'}>
+        <Link className={style.button} to={ROUTES.LANDING}>
            <FaRegCirclePlay />
         </Link>
         <img className={style.logo} src={logo} alt="logo-el-festin" />
